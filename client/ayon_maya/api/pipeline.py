e288392848081e58ffe5117cafad573e1a8f7b9e
Veena Vignale <veena.vignale@supamonks.lan>
2025-09-02 17:32:44 +0200

    Fix versioning and jean moulinette import

--- client/ayon_maya/api/pipeline.py
+++ client/ayon_maya/api/pipeline.py
@@ -664,8 +664,8 @@ def on_new():
 
     _remove_workfile_lock()
 
-    import jean_moulinette.lib as scripts_lib
-    scripts_lib.execute_script(get_current_project_name(), "initialize_workfile_from_template")
+    import jean_moulinette
+    jean_moulinette.lib.execute_script(get_current_project_name(), "initialize_workfile_from_template")
 
 
 def on_task_changed():
--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "maya"
 title = "Maya"
-version = "0.4.13_smks"
+version = "0.4.13+smks-0.0.1"
 app_host_name = "maya"
 client_dir = "ayon_maya"
 
