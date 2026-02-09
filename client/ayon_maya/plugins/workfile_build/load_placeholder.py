5b1e29bc6dd93c1e376e02455e2b7998c9998d6b
raphael.bandet <raphael.bandet@supamonks.com>
2026-02-09 16:38:39 +0100

    * fix: use super on MayaPlaceholderLoadMixin instead of copy and paste

--- client/ayon_maya/plugins/workfile_build/load_placeholder.py
+++ client/ayon_maya/plugins/workfile_build/load_placeholder.py
@@ -36,176 +36,32 @@ class MayaPlaceholderLoadMixin(PlaceholderLoadMixin):
                 plugins.
         """
 
-        loaders_by_name = self.builder.get_loaders_by_name()
-        loader_items = [
-            {"value": loader_name, "label": loader.label or loader_name}
-            for loader_name, loader in loaders_by_name.items()
-        ]
-
-        loader_items = list(sorted(loader_items, key=lambda i: i["label"]))
         group_modes_labels = [group_mode.fullname for group_mode in list(GroupMode)]
-        options = options or {}
 
-        # Get product types from all loaders excluding "*"
-        product_types = set()
-        for loader in loaders_by_name.values():
-            product_types.update(loader.product_types)
-        product_types.discard("*")
-
-        # Sort for readability
-        product_types = list(sorted(product_types))
-
-        builder_type_enum_items = [
-            {"label": "Current folder", "value": "context_folder"},
-            {"label": "Linked folders", "value": "linked_folders"},
-            {"label": "All folders", "value": "all_folders"},
-        ]
-
-        link_types = ayon_api.get_link_types(self.builder.project_name)
-
-        # Filter link types for folder to folder links
-        link_types_enum_items = [
-            {"label": link_type["name"], "value": link_type["linkType"]}
-            for link_type in link_types
-            if (
-                    link_type["inputType"] == "folder"
-                    and link_type["outputType"] == "folder"
-            )
-        ]
-
-        if not link_types_enum_items:
-            link_types_enum_items.append(
-                {"label": "<No link types>", "value": None}
-            )
-
-        build_type_label = "Folder Builder Type"
-        build_type_help = (
-            "Folder Builder Type\n"
-            "\nBuilder type describe what template loader will look"
-            " for."
-            "\nCurrent Folder: Template loader will look for products"
-            " of current context folder (Folder /assets/bob will"
-            " find asset)"
-            "\nAll folders: All folders matching the regex will be"
-            " used."
-        )
-
-        product_type = options.get("product_type")
-        if product_type is None:
-            product_type = options.get("family")
-
-        return [
-            attribute_definitions.UISeparatorDef(),
-            attribute_definitions.UILabelDef("Main attributes"),
-            attribute_definitions.UISeparatorDef(),
-
-            attribute_definitions.EnumDef(
-                "builder_type",
-                label=build_type_label,
-                default=options.get("builder_type"),
-                items=builder_type_enum_items,
-                tooltip=build_type_help
-            ),
-            attribute_definitions.EnumDef(
-                "link_type",
-                label="Link Type",
-                items=link_types_enum_items,
-                tooltip=(
-                    "Link Type\n"
-                    "\nDefines what type of link will be used to"
-                    " link the asset to the current folder."
+        result = super().get_load_plugin_options(options)
+        attributes_list = []
+        for attribute in result:
+            if attribute.label == "Loader":
+                attributes_list.append(
+                    attribute_definitions.EnumDef(
+                        "group_mode",
+                        label="Group mode",
+                        default=options.get("group_mode"),
+                        items=group_modes_labels,
+                        tooltip=(
+                            "Group Mode"
+                            "\nDefines how imported products will "
+                            "be placed in hierarchy\n"
+                            f"{GroupMode.Placeholder.fullname}: Places content at the placeholder's level, "
+                            f"no additional grouping\n"
+                            f"{GroupMode.Folders.fullname}: Groups products coming from same asset "
+                            f"under a group with folder's name"
+                        )
+                    )
                 )
-            ),
-            attribute_definitions.EnumDef(
-                "product_type",
-                label="Product type",
-                default=product_type,
-                items=product_types
-            ),
-            attribute_definitions.TextDef(
-                "representation",
-                label="Representation name",
-                default=options.get("representation"),
-                placeholder="ma, abc, ..."
-            ),
-            attribute_definitions.EnumDef(
-                "group_mode",
-                label="Group mode",
-                default=options.get("group_mode"),
-                items=group_modes_labels,
-                tooltip=(
-                    "Group Mode"
-                    "\nDefines how imported products will "
-                    "be placed in hierarchy\n"
-                    f"{GroupMode.Placeholder.fullname}: Places content at the placeholder's level, "
-                    f"no additional grouping\n"
-                    f"{GroupMode.Folders.fullname}: Groups products coming from same asset "
-                    f"under a group with folder's name"
-                )
-            ),
-            attribute_definitions.EnumDef(
-                "loader",
-                label="Loader",
-                default=options.get("loader"),
-                items=loader_items,
-                tooltip=(
-                    "Loader"
-                    "\nDefines what AYON loader will be used to"
-                    " load assets."
-                    "\nUseable loader depends on current host's loader list."
-                    "\nField is case sensitive."
-                )
-            ),
-            attribute_definitions.TextDef(
-                "loader_args",
-                label="Loader Arguments",
-                default=options.get("loader_args"),
-                placeholder='{"camera":"persp", "lights":True}',
-                tooltip=(
-                    "Loader"
-                    "\nDefines a dictionary of arguments used to load assets."
-                    "\nUseable arguments depend on current placeholder Loader."
-                    "\nField should be a valid python dict."
-                    " Anything else will be ignored."
-                )
-            ),
-            attribute_definitions.NumberDef(
-                "order",
-                label="Order",
-                default=options.get("order") or 0,
-                decimals=0,
-                minimum=0,
-                maximum=999,
-                tooltip=(
-                    "Order"
-                    "\nOrder defines asset loading priority (0 to 999)"
-                    "\nPriority rule is : \"lowest is first to load\"."
-                )
-            ),
-            attribute_definitions.UISeparatorDef(),
-            attribute_definitions.UILabelDef("Optional attributes"),
-            attribute_definitions.UISeparatorDef(),
-            attribute_definitions.TextDef(
-                "folder_path",
-                label="Folder filter",
-                default=options.get("folder_path"),
-                placeholder="regex filtering by folder path",
-                tooltip=(
-                    "Filtering assets by matching"
-                    " field regex to folder path"
-                )
-            ),
-            attribute_definitions.TextDef(
-                "product_name",
-                label="Product filter",
-                default=options.get("product_name"),
-                placeholder="regex filtering by product name",
-                tooltip=(
-                    "Filtering assets by matching"
-                    " field regex to product name"
-                )
-            ),
-        ]
+            attributes_list.append(attribute)
+
+        return attributes_list
 
 
 class MayaPlaceholderLoadPlugin(MayaPlaceholderPlugin, MayaPlaceholderLoadMixin):
