69eb4787c9c00d7e84b2c3559423b37253e43661
raphael.bandet <raphael.bandet@supamonks.com>
2026-02-26 15:25:53 +0100

    * fix: Fix when placeholder parent is None

--- client/ayon_maya/plugins/workfile_build/load_placeholder.py
+++ client/ayon_maya/plugins/workfile_build/load_placeholder.py
@@ -200,7 +200,7 @@ class MayaPlaceholderLoadPlugin(MayaPlaceholderPlugin, MayaPlaceholderLoadMixin)
                 worldSpace=True
             )
 
-            if not cmds.objExists(scene_parent):
+            if not scene_parent or not cmds.objExists(scene_parent):
                 name = scene_parent.split("|")[-1]
                 placeholder_parent = get_node_parent(placeholder.scene_identifier)
                 cmds.group(name=name, parent=placeholder_parent, empty=True)
--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "maya"
 title = "Maya"
-version = "0.5.3+smks-0.13.0"
+version = "0.5.3+smks-0.13.1"
 app_host_name = "maya"
 client_dir = "ayon_maya"
 project_can_override_addon_version = True
